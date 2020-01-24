ff9556ef955f55d48f0aa35fef61c427de79978e
costadefelipe@gmail.com <costadefelipe@gmail.com>
2020-01-24 12:48:47 -0300

    Criando o servidor da aplicação

--- server.js
+++ server.js
@@ -1,3 +1,24 @@
 'use strict'
 
-console.log("Testando...");
+const express = require('express');
+const http = require('http');
+const debug = require('debug')('nodestr:server');
+
+const app = express();
+const port = 3000;
+app.set('port', port);
+
+const server = http.createServer(app);
+const router = express.Router(); 
+
+var route = router.get('/', (req,res,next) => {
+        res.status(200).send({
+            title:'Node TODO api',
+            version:'0.0.1'
+        });
+});
+
+app.use('/', route);
+
+server.listen(port);
+console.log('API rodando na porta:' + port);
