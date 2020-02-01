d96ed83918119f5cbc901562dd246deb7ab63d50
costadefelipe@gmail.com <costadefelipe@gmail.com>
2020-02-01 11:17:53 -0300

    add nodemon / separanda e criando as rotas

--- src/app.js
+++ src/app.js
@@ -1,18 +1,21 @@
 'use strict';
 
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 const router = express.Router();
 
-const route = router.get('/', (req, res, next) => {
-    res.status(200).send({
-        title:"Node Todo API",
-        version:"0.0.4"
-    });
+//carrega as rotas
+const indexRouter = require('./routes/index.router');
+const todoRouter = require('./routes/todo.router');
 
-});
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ 
+    extended: false 
+}));
 
-app.use('/', route);
+app.use('/', indexRouter);
+app.use('/todo', todoRouter);
 
 module.exports = app;
