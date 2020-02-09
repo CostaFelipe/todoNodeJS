206d5d583fbbaeeb7f82d018e4e9d7fc15c883a1
costadefelipe@gmail.com <costadefelipe@gmail.com>
2020-02-09 15:54:33 -0300

    criando middleware de teste do servidor

--- bin/server.js
+++ bin/server.js
@@ -2,9 +2,11 @@
 
 const app = require('../src/app');
 const http = require('http');
-const debug = require('debug')('nodestr:server');
 
-const port = normalizePort(process.env.PORT || '3000');
+const middlewares = require('./middlewares/middlewares.server');
+
+const port = process.env.PORT || '3000';
+app.use(middlewares.normalizePort);
 app.set('port', port);
 
 const server = http.createServer(app);
@@ -13,50 +15,5 @@ server.listen(port, () => {
     console.log(`Listening at http://localhost:${port}`);
 });
 
-server.on('error', onError);
-server.on('listening', onListening);
-
-function normalizePort(val){
-    const port = parseInt(val, 10);
-
-    if(isNaN(port)){
-        return val;
-    }
-    if(port >= 0){
-        return port;
-    }
-
-    return false;
-}
-
-function onError(error){
-    if(error.syscall !== 'listen'){
-        throw error;
-    }
-
-    const bind = typeof port === 'string' ?
-        'Pipe' + port:
-        'Port' + port;
-
-    switch (error.code){
-        case 'EACCES':
-            console.error(bind + 'requires elevated privileges');
-            process.exit(1);
-            break;
-        case 'EADDRINUSE':
-            console.error(bind + ' is already in use');
-            process.exit(1);
-            break;
-        default:
-            throw error;
-    }
-}
-
-function onListening(){
-    const addr = server.address();
-    const bind = typeof addr === 'string'
-        ? 'pipe' + addr
-        : 'port' + addr.port;
-    debug('Listening on' + bind);
-}
-
+app.on('error', middlewares.onError);
+app.on('listening', middlewares.onListening);
