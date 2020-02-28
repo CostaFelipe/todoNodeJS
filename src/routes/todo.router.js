a311b5f67e02e4cd8b01b1c03f22d72c5a27f811
costadefelipe@gmail.com <costadefelipe@gmail.com>
2020-02-28 14:26:22 -0300

    commit nas rotas

--- src/routes/todo.router.js
+++ src/routes/todo.router.js
@@ -4,8 +4,22 @@ const express = require('express');
 const router = express.Router();
 const controller = require('../controllers/todo.controller');
 
+/**
+  * @endpoint: /todo/
+  * @method: POST
+**/
 router.post('/', controller.post);
+
+/**
+  * @endpoint: /todo/:id
+  * @method: PUT
+**/
 router.put('/:id', controller.put);
+
+/**
+  * @endpoint: /todo/
+  * @method: DELETE
+**/
 router.delete('/', controller.delete);
 
 module.exports = router;
